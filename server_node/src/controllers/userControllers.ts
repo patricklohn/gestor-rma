0f29d1c5329373587800a2f512f8ac770f46a7d7
patricklohn <patricklohn3@gmail.com>
2025-04-12 23:46:54 -0300

    criação de permissão dentro do server #2

--- server_node/src/controllers/userControllers.ts
+++ server_node/src/controllers/userControllers.ts
@@ -6,11 +6,22 @@ const prisma = new PrismaClient();
 
 async function createUser(req: Request,res: Response){
     try {
-        const {name, email, password} = req.body;
-        if(!name || !email || !password){
+        const {name, email, password, user} = req.body;
+        if(!name || !email || !password || !user){
             res.status(400).json({message: "Os campos do formulario são obrigatorios!"})
             return
         }
+
+        const userData = await prisma.user.findUnique({where:{uuid: user}})
+        if(!userData){
+            res.status(400).json({message: "Usuario para fazer a função não identificado"})
+            return
+        }
+        if(userData.role != "Adm"){
+            res.status(400).json({message: "Usuario não é autorizado a efetuar a função"})
+            return
+        }
+
         const hashedPassword = await cryptPassword(password);
         const userCreate = await prisma.user.create({
             data:{
