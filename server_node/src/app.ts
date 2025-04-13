b3b44c8e39e8e455263fc9fffd6b3b4d68926d32
patricklohn <patricklohn3@gmail.com>
2025-04-13 13:22:29 -0300

    Configurado multer para acompanhamento de requisições no log

--- server_node/src/app.ts
+++ server_node/src/app.ts
@@ -1,10 +1,13 @@
 import express from "express";
 import cors from 'cors'; 
 import routes from "./routes/routes";
+import morgan from "morgan"; 
 
 const app = express();
 app.use(cors());
 app.use(express.json());
+app.use(express.urlencoded({extended: true}))
+app.use(morgan('dev'));
 
 //Rotas
 app.use("/", routes);
