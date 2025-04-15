f3b96ce20859d5b33d250a7f55b90abf84b4d466
patricklohn <patricklohn3@gmail.com>
2025-04-15 07:56:52 -0300

    Criação das rotas com ReacRouter Doom

--- app_frontend/src/main.tsx
+++ app_frontend/src/main.tsx
@@ -2,9 +2,28 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
+import {createBrowserRouter, RouterProvider} from 'react-router-dom'
 
+//Pages
+import Home from './routes/Home.tsx'
+import ErrorPage from './routes/ErrorPage.tsx'
+
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    errorElement: <ErrorPage/>,
+    children:[
+      {
+        path: "/",
+        element: <Home/>
+      },
+    ]
+  }
+])
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router}/>
   </StrictMode>,
 )
