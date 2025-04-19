9ff16d0e8d02193330503a5e06efee24b094db91
patricklohn <patricklohn3@gmail.com>
2025-04-19 20:00:30 -0300

    configurado para envio de forms

--- app_frontend/src/routes/Login.tsx
+++ app_frontend/src/routes/Login.tsx
@@ -1,7 +1,33 @@
 import classes from './Login.module.css'
 import logo from '../assets/LogoA.png'
+import { useState } from 'react'
+import { useNavigate } from 'react-router-dom'
+import { toast } from 'react-toastify'
+import RmaApi from '../axios/config'
 
 const Login = () => {
+
+  const[email, setEmail] = useState('');
+  const [senha, setSenha] = useState('');
+  const navigate = useNavigate();
+
+  const handleSubmit = async(e: React.FormEvent) =>{
+    e.preventDefault();
+    try {
+      const response = await RmaApi.post('/login/userLogin', {email, senha});
+      if(response.status === 200){
+        const { token} = response.data;
+        localStorage.setItem('token', token);
+        toast.success("Login realizado com sucesso!")
+        navigate("/")
+      }
+      
+    } catch (error) {
+      console.log(error)
+      toast.error("Algum erro ocorreu ao tentar efetuar o Login!")
+    }
+  }
+
   return (
     <div className={classes.login_container}>
       <div className={classes.login_logo_container}>
@@ -10,14 +36,14 @@ const Login = () => {
         <img src={logo} alt='Imagem Logo'/>
       </div>
       <div className={classes.login_form_container}>
-        <form action="">
+        <form onSubmit={handleSubmit}>
           <div className={classes.login_formInput_container}>
             <label htmlFor="">Email</label>
-            <input type="email" required />
+            <input type="email" value={email} onChange={(e) => setEmail(e.target.value)} required />
           </div>
           <div className={classes.login_formInput_container}>
             <label htmlFor="">Senha</label>
-            <input type="password" required />
+            <input type="password" value={senha} onChange={(e) => setSenha(e.target.value)} required />
           </div>
           <input type="submit" value="Entrar"/>
         </form>
