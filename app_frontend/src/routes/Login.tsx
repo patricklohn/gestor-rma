debc86d95c48abdb6c7d6c54d421d4f5854ddf73
patricklohn <patricklohn3@gmail.com>
2025-04-19 19:23:53 -0300

    Criado tela de login

--- app_frontend/src/routes/Login.tsx
+++ app_frontend/src/routes/Login.tsx
@@ -1,16 +1,25 @@
+import classes from './Login.module.css'
+import logo from '../assets/LogoA.png'
+
 const Login = () => {
   return (
-    <div>
-      <div>
+    <div className={classes.login_container}>
+      <div className={classes.login_logo_container}>
+        <h1>Login</h1>
+        <p>Entre com o seu usuario</p>
+        <img src={logo} alt='Imagem Logo'/>
+      </div>
+      <div className={classes.login_form_container}>
         <form action="">
-          <div className="">
+          <div className={classes.login_formInput_container}>
             <label htmlFor="">Email</label>
             <input type="email" required />
           </div>
-          <div className="">
+          <div className={classes.login_formInput_container}>
             <label htmlFor="">Senha</label>
             <input type="password" required />
           </div>
+          <input type="submit" value="Entrar"/>
         </form>
       </div>
     </div>
