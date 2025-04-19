e4f036d03649d1f2d46092cfba9c7f10f9f80ee4
patricklohn <patricklohn3@gmail.com>
2025-04-19 19:43:05 -0300

    configurado toastify

--- app_frontend/src/App.tsx
+++ app_frontend/src/App.tsx
@@ -1,10 +1,13 @@
 import './App.css'
 import { Outlet } from 'react-router-dom'
+import {ToastContainer} from 'react-toastify'
+import 'react-toastify/dist/ReactToastify.css'
 
 function App() {
 
   return (
     <div className='App'>
+      <ToastContainer position="top-right" autoClose={5000} hideProgressBar={false} newestOnTop={false} closeOnClick rtl={false} pauseOnFocusLoss draggable pauseOnHover theme="light"/>
       <div className='container-body'>
         <Outlet/>
       </div>
