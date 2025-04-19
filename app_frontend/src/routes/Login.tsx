48bfbbe39e579e8c1d29b895b7141ca60b412e3f
patricklohn <patricklohn3@gmail.com>
2025-04-18 23:43:48 -0300

    Iniciando form de login

--- app_frontend/src/routes/Login.tsx
+++ app_frontend/src/routes/Login.tsx
@@ -1,7 +1,18 @@
 const Login = () => {
   return (
     <div>
-      
+      <div>
+        <form action="">
+          <div className="">
+            <label htmlFor="">Email</label>
+            <input type="email" required />
+          </div>
+          <div className="">
+            <label htmlFor="">Senha</label>
+            <input type="password" required />
+          </div>
+        </form>
+      </div>
     </div>
   )
 }
