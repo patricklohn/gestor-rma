b666d9e0314cefc952ca014976f16bebaa89297b
patricklohn <patricklohn3@gmail.com>
2025-04-25 22:13:53 -0300

    inicialização da estrutura para pagina RMA

--- app_frontend/src/routes/Rma.tsx
+++ app_frontend/src/routes/Rma.tsx
@@ -1,13 +1,23 @@
 import React from 'react'
 import classes from './Rma.module.css'
 import NavBar from '../components/NavBar'
+import {useNavigate} from "react-router-dom"
 
 const Rma = () => {
+  const navigate = useNavigate();
+
   return (
     <div className={classes.rma}>
       <NavBar/>
       <div className={classes.rma_container}>
         <h1>Rma</h1>
+        <div className={classes.rma_create}>
+          <h2>Criação de RMA</h2>
+          <p>Para criar um novo RMA, clique no botão abaixo.</p>
+          <button onClick={() => navigate('rma/create')} className='rma_btn_create'>
+            Criar Rma
+          </button>
+        </div>
       </div>
     </div>
   )
