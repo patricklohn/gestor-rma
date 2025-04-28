335c82f1d1c060944c692988e3c6599c965f0a53
patricklohn <patricklohn3@gmail.com>
2025-04-28 07:41:29 -0300

    criação getAll com frontEnd

--- app_frontend/src/routes/Rma.tsx
+++ app_frontend/src/routes/Rma.tsx
@@ -1,15 +1,46 @@
 import classes from './Rma.module.css'
 import NavBar from '../components/NavBar'
 import {useNavigate} from "react-router-dom"
-import { useState, useEffect} from 'react'
+import { useState, useEffect, useRef} from 'react'
 import { toast } from 'react-toastify'
 import RmaApi from '../axios/config'
 import axios from 'axios'
 
 const Rma = () => {
   const navigate = useNavigate();
-  const [rmaData, setRamaData] = useState([])
+  const [rmaData, setRmaData] = useState([])
   const [searchTerm, setSearchTerm] = useState('');
+  const isFetching = useRef(false);
+  
+  const loadRma = async () =>{
+    if (isFetching.current) return;
+    isFetching.current = true;
+    
+    try {
+      const res = await RmaApi.get('warranty/getAll');
+      if(!res.data){
+        toast.error('Nenhum RMA encontrado!')
+      }
+      setRmaData(res.data);
+      isFetching.current = false;
+    } catch (error: unknown){
+      if (axios.isAxiosError(error)) {
+        toast.error(error.response?.data?.message || 'Erro da API');
+      } else if (error instanceof Error) {
+        toast.error(error.message);
+      } else {
+        toast.error('Erro desconhecido');
+      }
+      setTimeout(() => {
+        isFetching.current = false;
+        loadRma();
+      }, 15000);
+    };
+  }
+
+  useEffect(()=>{
+    loadRma();
+  }, []);
 
   return (
     <div className={classes.rma}>
@@ -19,7 +50,7 @@ const Rma = () => {
         <div className={classes.rma_create}>
           <h2>Criação de RMA</h2>
           <p>Para criar um novo RMA, clique no botão abaixo.</p>
-          <button onClick={() => navigate('rma/create')} className={classes.rma_btn_create}>
+          <button onClick={() => navigate('#')} className={classes.rma_btn_create}>
             Criar Rma
           </button>
         </div>
