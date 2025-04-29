3c5a81a0e5947030d1cf682a32ca22b6804759ea
patricklohn <patricklohn3@gmail.com>
2025-04-28 21:36:24 -0300

    Inicio da table de RMA

--- app_frontend/src/routes/Rma.tsx
+++ app_frontend/src/routes/Rma.tsx
@@ -42,6 +42,17 @@ const Rma = () => {
     loadRma();
   }, []);
 
+  const filterRma = rmaData.filter((rma: any) =>{
+    rma.description.toLowerCase().includes(searchTerm.toLowerCase()) ||
+    rma.serial_number.toLowerCase().includes(searchTerm.toLowerCase()) ||
+    rma.product.toLowerCase().includes(searchTerm.toLowerCase()) ||
+    rma.supplier.toLowerCase().includes(searchTerm.toLowerCase()) ||
+    rma.client.toLowerCase().includes(searchTerm.toLowerCase()) ||
+    rma.invoice.toLowerCase().includes(searchTerm.toLowerCase()) ||
+    rma.status.toLowerCase().includes(searchTerm.toLowerCase()) ||
+    rma.order_service.toLowerCase().includes(searchTerm.toLowerCase())
+  })
+
   return (
     <div className={classes.rma}>
       <NavBar/>
@@ -56,8 +67,28 @@ const Rma = () => {
         </div>
         <div className={classes.rma_list}>
           <h2>Lista Rma</h2>
-
+          {isFetching.current === true && <div id="loading-screen"><div className="loading-spinner"></div></div>}
+          {!filterRma.length && <p>Não a cadastrados</p>}
         </div>
+        <div className={classes.produtos_search}>
+            <input
+            type="text"
+            placeholder="Pesquisar..."
+            value={searchTerm}
+            onChange={(e) => setSearchTerm(e.target.value)}/>
+        </div>
+        {filterRma.length > 0 && (
+          <table>
+            <tr>
+              <th>Descrição</th>
+              <th>Data Inicio</th>
+              <th>Data Finalização</th>
+              <th>Produto de loja</th>
+              <th>Fornecedor</th>
+              <th>Cliente</th>
+            </tr>
+          </table>
+        )}
       </div>
     </div>
   )
